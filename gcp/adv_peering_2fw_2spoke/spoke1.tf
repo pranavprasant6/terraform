38b84922f44c4641286f8d45d5b8ad67737ac208
pranavprasant6 <64271113+pranavprasant6@users.noreply.github.com>
2020-12-08 12:44:24 +0530

    Update spoke1.tf

--- gcp/adv_peering_2fw_2spoke/spoke1.tf
+++ gcp/adv_peering_2fw_2spoke/spoke1.tf
@@ -22,25 +22,25 @@ module "vpc_spoke1" {
   }
 }
 
-module "vm_spoke1" {
-  source          = "./modules/create_vm/"
-  vm_names        = ["spoke1-vm1", "spoke1-vm2"]
-  vm_zones        = ["${var.region}-a", "${var.region}-a"]
-  vm_machine_type = "f1-micro"
-  vm_image        = "ubuntu-os-cloud/ubuntu-1604-lts"
-  vm_subnetworks  = ["${module.vpc_spoke1.subnetwork_self_link[0]}", "${module.vpc_spoke1.subnetwork_self_link[0]}"]
-  vm_ssh_key      = "ubuntu:${var.ubuntu_ssh_key}"
-  startup_script  = "${file("${path.module}/scripts/webserver-startup.sh")}"                                         // default "" - runs no startup script
+#module "vm_spoke1" {
+#  source          = "./modules/create_vm/"
+#  vm_names        = ["spoke1-vm1", "spoke1-vm2"]
+#  vm_zones        = ["${var.region}-a", "${var.region}-a"]
+#  vm_machine_type = "f1-micro"
+#  vm_image        = "ubuntu-os-cloud/ubuntu-1604-lts"
+#  vm_subnetworks  = ["${module.vpc_spoke1.subnetwork_self_link[0]}", "${module.vpc_spoke1.subnetwork_self_link[0]}"]
+#  vm_ssh_key      = "ubuntu:${var.ubuntu_ssh_key}"
+#  startup_script  = "${file("${path.module}/scripts/webserver-startup.sh")}"                                         // default "" - runs no startup script
 
-  internal_lb_create = true           // default false
-  internal_lb_name   = "spoke1-intlb" // default "intlb"
-  internal_lb_ports  = ["80", "443"]  // default ["80"]
-  internal_lb_ip     = "10.10.1.100"  // default "" (assigns an any available IP in subnetwork )
+#  internal_lb_create = true           // default false
+#  internal_lb_name   = "spoke1-intlb" // default "intlb"
+#  internal_lb_ports  = ["80", "443"]  // default ["80"]
+#  internal_lb_ip     = "10.10.1.100"  // default "" (assigns an any available IP in subnetwork )
 
-  providers = {
-    google = "google.spoke1"
-  }
-}
+#  providers = {
+#    google = "google.spoke1"
+#  }
+#}
 
 #************************************************************************************
 # CREATE PEERING LINK SPOKE1-to-TRUST
